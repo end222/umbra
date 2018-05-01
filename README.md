d8d192118bf05ac24c1faff79542d868d0e3e826
end222 <pabloorduna98@gmail.com>
2018-05-01 17:20:08 +0200

    Update README

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# umbra
-Umbra AUR helper
+# Umbra
+## What is Umbra?
+Umbra is an AUR helper for Arch Linux which is written in Ruby
+
+## Why another AUR helper?
+Umbra exists mainly because I wanted to make a more serious programming project mainly to practice and improve. I do not have any particular reason why it is better than other AUR helpers.
+
+The main difference is that Umbra is written in Ruby, a language that I really like and, as far as I know, hasn't been used in other helpers so I wanted to see how it resulted.
+
+## Dependencies
+* Ruby
+* Colorize gem (gem install colorize)
--- umbra.rb
+++ umbra.rb
@@ -7,21 +7,28 @@ version=0.1
 
 if ARGV.length >= 1
 	# Install new package
-	if ARGV[0] == "-S"
+	case ARGV[0]
+	when "-S"
 		if ARGV.length < 2
 			puts "Usage: umbra -S <package_name>"
 		else
 			puts "Checking if the package is already available in the official repositories..."
 			%x(sudo pacman -S #{ARGV[1]})
 		end
-	elsif ARGV[0] == "-Syu"
+	when "-Syu"
 		# TODO: Update AUR packages too
 		puts "Updating official packages..."
 		%x(sudo pacman -Syu)
+	# Show help page
+	when "-h", "--help"
+		puts "Help page"
+		# TODO: write some help
 	# Show package version
-	elsif ARGV[0] == "-v"
+	when "-v"
 		puts "Umbra AUR helper, version #{version}"
 		puts "Developed by end222"
+	else
+		puts "Operation not recognized"
 	end
 else
 	puts "No operation code was given"
